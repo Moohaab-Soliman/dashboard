ef84ff8ebf14de462be8ff799c674a4d3fd1ca4c
Moohab-Soliman <mohab.soliman@tu-clausthal.de>
2020-10-25 19:12:01 +0100

    your message

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+
+import Login from "./views/Login";
+// import AdminLayout from "layouts/Admin.jsx";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import Admin from "./layouts/Admin";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Admin} />
+          <Route
+            path="/login"
+            render={(props) => {
+              // console.log(currentUser);
+              var cond = true;
+              if (cond) {
+                return <Login />;
+              } else {
+                return <Redirect from="/" to="/admin12/dashboard" />;
+              }
+            }}
+          />
+
+          <Route path="/admin/" render={(props) => <Admin {...props} />} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -27,8 +27,9 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 import Login from "./views/Login";
 import { AuthProvider } from "./Auth";
-
+import AdminLayout from "layouts/Admin.jsx";
 import Admin from "layouts/Admin.jsx";
+import PrivateRoute from "./PrivateRoute";
 
 ReactDOM.render(
   <AuthProvider>
@@ -37,6 +38,7 @@ ReactDOM.render(
         <Route
           path="/login"
           render={(props) => {
+            // console.log(currentUser);
             var cond = true;
             if (cond) {
               return <Login />;
